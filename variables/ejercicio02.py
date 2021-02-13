354c927b9b1c83f60ccd069f230ec4a6a820bd41
bl00p1ng <blooping@protonmail.com>
2021-02-13 12:21:02 -0500

    Variables: ejercicio 2

---
+++ variables/ejercicio02.py
@@ -0,0 +1,11 @@
+def run():
+    # Ejercicio 2
+    # Crea la variable nombre y asígnale tu nombre completo. Muestra su valor por
+    # pantalla
+
+    name = 'Andrés Felipe López Molina'
+    print(name)
+
+
+if __name__ == '__main__':
+    run()
