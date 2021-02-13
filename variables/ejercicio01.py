fd6a0e42181a4918a0cfcd3c36c0a79719d28283
bl00p1ng <blooping@protonmail.com>
2021-02-13 12:17:08 -0500

    Variables: ejercicio 1

---
+++ variables/ejercicio01.py
@@ -0,0 +1,19 @@
+def run():
+    # Ejercicio 1
+    # Escribe un programa en el que se declaren las variables enteras x e y. Asignales
+    # los valores 144 y 999 respectivamente. A continuación, muestra por pantalla
+    # el valor de cada variable, la suma, la resta, la división y la multiplicación.
+
+    x = 144
+    y = 999
+
+    print('El valor de x es: ' + str(x))
+    print('El valor de y es: ' + str(y))
+    print('x + y = ' + str(x + y))
+    print('x - y = ' + str(x - y))
+    print('x / y = ' + str(x / y))
+    print('x * y = ' + str(x * y))
+
+
+if __name__ == '__main__':
+    run()
