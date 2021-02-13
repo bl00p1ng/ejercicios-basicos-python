a788ba02fe823a979a463e685c2287b1609c5b75
bl00p1ng <blooping@protonmail.com>
2021-02-13 12:36:05 -0500

    Variables: ejercicio 4

---
+++ variables/ejercicio04.py
@@ -0,0 +1,16 @@
+def run():
+    # Ejercicio 4
+    # Realiza un conversor de euros a dólar. La cantidad en euros que se quiere
+    # convertir deberá estar almacenada en una variable.
+
+    eur_in_usd = 1.21
+
+    print('💱 Conversor de Euros a Dólares')
+    user_eur = float(input(':: Ingresa la cantidad de Euros que quieres  convertir: '))
+
+    eur_converted = user_eur * eur_in_usd
+    print(str(user_eur) + ' 💶 Euros equivalen a $' + str(eur_converted) + ' 💵 Dólares')
+
+
+if __name__ == '__main__':
+    run()
