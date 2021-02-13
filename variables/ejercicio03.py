ddafaa083e18ac8ee22e7dc331d30f2c457ea1c4
bl00p1ng <blooping@protonmail.com>
2021-02-13 12:26:16 -0500

    Variables: ejercicio 3

---
+++ variables/ejercicio03.py
@@ -0,0 +1,16 @@
+def run():
+    # Ejercicio 3
+    # Crea las variables nombre, direction y teléfono y asígnale los valores correspondientes. Muestra los valores de
+    # esas variables por pantalla de tal forma que el resultado del programa sea el mismo que en el ejercicio 2.
+
+    name = 'Andrés Felipe López'
+    phone = '509-684-1752'
+    address = '2000  Calico Drive, Colville WA'
+
+    print('Nombre: ' + name)
+    print('Teléfono: ' + phone)
+    print('Dirección: ' + address)
+
+
+if __name__ == '__main__':
+    run()
