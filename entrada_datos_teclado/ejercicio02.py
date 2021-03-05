39ffa49eb36d77eb0116772667c2a2eff4d9ca1d
bl00p1ng <blooping@protonmail.com>
2021-03-05 13:54:40 -0500

    Entrada de datos teclado: ejercicio 02

---
+++ entrada_datos_teclado/ejercicio02.py
@@ -0,0 +1,17 @@
+def run():
+    # Ejercicio 4
+    # Escribe un programa que sume, reste, multiplique y divida dos números
+    # introducidos por teclado.
+
+    print('**** Operaciones básicas ****')
+    number1 = int(input(':: Escribe un número: '))
+    number2 = int(input(':: Escribe otro número: '))
+
+    print(f'El resultado de {number1} + {number2} es {number1 + number2}')
+    print(f'EL resultado de {number1} - {number2} es {number1 - number2}')
+    print(f'EL resultado de {number1} x {number2} es {number1 * number2}')
+    print(f'EL resultado de {number1} ➗ {number2} es {number1 //number2}')
+
+
+if __name__ == '__main__':
+    run()
