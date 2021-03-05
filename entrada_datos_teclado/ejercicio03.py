2f4bac552b501e3807810b401b2548277c6880ac
bl00p1ng <blooping@protonmail.com>
2021-03-05 14:02:32 -0500

    Entrada de datos teclado: ejercicio 03

---
+++ entrada_datos_teclado/ejercicio03.py
@@ -0,0 +1,13 @@
+def run():
+    # Ejercicio 3
+    # Escribe un programa que calcule el área de un rectángulo.
+
+    print('**** Área de un rectángulo ****')
+    width = int(input(':: Escribe la base del rectángulo: '))
+    height = int(input(':: Escribe la altura del rectángulo: '))
+
+    print(f'El área del triángulo es {width * height}')
+
+
+if __name__ == '__main__':
+    run()
